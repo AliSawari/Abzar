832cccf522cbc2cd477baff99984f0898df658e8
Ali Sawari <ali.dragon.swr@gmail.com>
2022-12-11 21:27:46 +0300

    chore(): cleaning up

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import Chalker from './chalker'
 import Copy from './copy'
 import Exclude from './exclude'
 import isThere from './isThere'
 import Paginator from './paginator'
 
-export {Chalker,Copy,Exclude,isThere,Paginator}
+export { Copy, Exclude, isThere, Paginator }
