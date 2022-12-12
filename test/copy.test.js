0b57ddb9bcbc3acce5b34ee29e9065d158e662cb
Ali Sawari <ali.dragon.swr@gmail.com>
2022-12-12 22:41:11 +0300

    ci: testing new features

--- test/copy.test.js
+++ test/copy.test.js
@@ -1,15 +1,23 @@
-const { Copy } = require("../lib")
+const { Copy } = require("../lib");
 
-describe("It should copy an object successfully", () => {
+const ali = {
+  name: "Ali",
+  age: 24,
+  job: "developer"
+}
+
+describe("All Features of the Copy function", () => {
   test("Copying a simple non-nested obj", () => {
-    let ali = {
-      name: "Ali",
-      age: 23
-    }
-
     let reza = Copy(ali)
     reza.name = "reza";
     expect(ali.name).toBe("Ali")
     expect(reza.name).toBe("reza")
+  })
+
+  test("Changing the attributes while copying", () => {
+    let hamid = Copy(ali, { name: "hamid", age: 47 });
+    expect(hamid.name).toBe("hamid")
+    expect(hamid.age).toBe(47);
+    expect(ali.name).toBe("Ali")
   })
 })
