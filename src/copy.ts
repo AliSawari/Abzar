cf6d598dc6820ae6e7bfbed36b8222181e23d6b0
Ali Sawari <ali.dragon.swr@gmail.com>
2022-12-12 22:40:57 +0300

    doc: better typing

--- src/copy.ts
+++ src/copy.ts
@@ -1,7 +1,7 @@
 export default function Copy<T extends Object>(origin: T, toChange?: T): T {
   const strings = JSON.stringify(origin);
   const newObj = new Object(JSON.parse(strings));
-  if (Object.values(toChange).length) {
+  if (toChange != null && Object.values(toChange).length) {
     for (let prop in toChange) {
       newObj[prop as string] = toChange[prop];
     }
